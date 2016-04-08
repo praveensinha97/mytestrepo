a87b51561eb2dd7ac8c2787bf4cfd4eb18201ccd
praveensinha97 <praveensinha97@yahoo.in>
2016-04-08 23:13:24 +0530

    added new project

---
+++ MvcRazorToPdfExample/Models/PdfExample.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcRazorToPdfExample.Models
+{
+    public class PdfExample
+    {
+        public string Heading { get; set; }
+        // my new changes
+        public IEnumerable<BasketItem> Items { get; set; }
+    }
+}
